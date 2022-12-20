1b07f92a12e31fbd8f383c9e39e87056c3941839
Yuvaraj K <yuvarajsomavamshi@gmail.com>
2022-12-20 14:37:26 +0530

    sync

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+docker compose -f "docker-compose.yml" down
 docker build --pull --rm -f "studio/Dockerfile" -t vinashak/studio:latest "studio"
-#docker build --pull --rm -f "docs/Dockerfile" -t vinashak/docs:latest "docs"
+docker build --pull --rm -f "docs/Dockerfile" -t vinashak/docs:latest "docs"
 docker compose -f "docker-compose.yml" up -d --build
