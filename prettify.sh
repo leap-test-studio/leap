0766adf796740a8508320318583946a14167a8b4
Yuvaraj K <yuvarajsomavamshi@gmail.com>
2023-01-06 17:26:30 +0530

    prettify

---
+++ prettify.sh
@@ -0,0 +1,12 @@
+AUTOMATION_HOME=`pwd`;
+cd $AUTOMATION_HOME/agent;
+npm run prettier:fix
+
+cd $AUTOMATION_HOME/orchestrator;
+npm run prettier:fix
+
+cd $AUTOMATION_HOME/studio;
+npm run prettier:fix
+
+cd $AUTOMATION_HOME/docs;
+npm run dev:prettify
