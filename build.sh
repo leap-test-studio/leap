1a99de3894d1ebd44e0825084960c36f39864038
Yuvaraj K <yuvarajsomavamshi@gmail.com>
2023-01-20 10:14:12 +0000

    sync

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 docker compose -f "docker-compose.yml" down
 docker image rm vinashak/studio vinashak/docs vinashak/orchestrator vinashak/client vinashak/agent
-docker build --pull --rm -f "studio/Dockerfile" -t vinashak/studio:latest "studio"
-docker build --pull --rm -f "docs/Dockerfile" -t vinashak/docs:latest "docs"
+docker build --pull -f "studio/Dockerfile" -t vinashak/studio:latest "studio"
+docker build --pull -f "docs/Dockerfile" -t vinashak/docs:latest "docs"
 docker compose -f "docker-compose.yml" up -d --build
