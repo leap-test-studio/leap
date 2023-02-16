27d02d890ee48428df65fb5affda71dbe7de21ae
Yuvaraj K <yuvarajsomavamshi@gmail.com>
2023-02-17 01:42:08 +0530

    screenshot

--- init.sh
+++ init.sh
@@ -1,8 +1,4 @@
 AUTOMATION_HOME=`pwd`;
-cd $AUTOMATION_HOME/agent;
-npm install;
-npm run prettier:fix
-npm run dev:upgrade
 
 cd $AUTOMATION_HOME/orchestrator;
 npm install;
@@ -12,9 +8,4 @@ npm run dev:upgrade
 cd $AUTOMATION_HOME/studio;
 npm install;
 npm run prettier:fix
-npm run dev:upgrade
-
-cd $AUTOMATION_HOME/docs;
-npm install;
-npm run dev:prettify
 npm run dev:upgrade
--- prettify.sh
+++ prettify.sh
@@ -1,10 +1,12 @@
 AUTOMATION_HOME=`pwd`;
 
 cd $AUTOMATION_HOME/orchestrator;
-npm run prettier:fix
+npm run prettier:fix &
 
 cd $AUTOMATION_HOME/studio;
-npm run prettier:fix
+npm run prettier:fix &
 
 cd $AUTOMATION_HOME/docs;
-npm run dev:prettify
+npm run dev:prettify &
+
+wait
