c1f877087d6b5265a9a720574e67a713587d615d
Yuvaraj K <yuvarajsomavamshi@gmail.com>
2024-06-20 12:23:20 +0530

    upgrade redux

--- studio/src/redux/Store.js
+++ studio/src/redux/Store.js
@@ -1,5 +1,5 @@
-import thunk from "redux-thunk";
-import { createStore, applyMiddleware, compose } from "redux";
+import { thunk } from "redux-thunk";
+import { legacy_createStore as createStore, applyMiddleware, compose } from "redux";
 import Reducers from "./reducers";
 
 const initialState = {};
