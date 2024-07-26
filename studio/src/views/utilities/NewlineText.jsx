5d93465937323ff8a528d66ac4a71511e1131385
Yuvaraj K <yuvaraj.krishnaraju@rakuten.com>
2024-07-26 13:29:57 +0530

    TPD Upgrade

--- studio/src/views/utilities/NewlineText.jsx
+++ studio/src/views/utilities/NewlineText.jsx
@@ -3,7 +3,11 @@ import isEmpty from "lodash/isEmpty";
 export const NewlineText = ({ text, className, style }) => {
   let child = "";
   if (!isEmpty(text)) {
-    child = text.split("\n").map((str, i) => <p key={i}>{str}</p>);
+    child = text.split("\n").map((str, i) => (
+      <p key={i} className="mb-2">
+        {str}
+      </p>
+    ));
   }
   return (
     <div className={className} style={style}>
